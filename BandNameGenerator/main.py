77ef74887739a5e0704e85ab71e1a2040f86e3cd
Andreaeduard Magri <247417302+Eiphilim-dev@users.noreply.github.com>
2025-12-11 20:20:30 +0100

    Improve band name generator with input prompts

--- BandNameGenerator/main.py
+++ BandNameGenerator/main.py
@@ -1,7 +1,14 @@
+# print("Welcome to the Band Name Generator")
+# print("What's the name of your favorite city?")
+# fav_city = input()
+# print("What's your pet's name?")
+# pet_name = input()
+# band_name = fav_city + " " + pet_name
+# print("Your band name is: " + band_name)
+
+#Or this
 print("Welcome to the Band Name Generator")
-print("What's the name of your favorite city?")
-fav_city = input()
-print("What's your pet's name?")
-pet_name = input()
+fav_city = input("What's the name of your favorite city?")
+pet_name = input("What's your pet's name?")
 band_name = fav_city + " " + pet_name
 print("Your band name is: " + band_name)
