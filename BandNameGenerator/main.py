d0632092a18e848615305976198cfa5a1f2599f0
Andreaeduard Magri <247417302+Eiphilim-dev@users.noreply.github.com>
2025-12-11 19:30:40 +0100

    first commit

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+print("What's the name of your favorite city?")
+fav_city = input()
+print("What's your pet's name?")
+pet_name = input()
+band_name = fav_city + " " + pet_name
+print("Your band name is: " + band_name)
